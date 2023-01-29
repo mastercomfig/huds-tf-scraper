4b8657657f718a36fa6299aa8b8b4c257387e39c
mastercoms <mastercoms@tuta.io>
2023-01-28 22:27:18 -0500

    upgrade megalist parser to handle albums better

--- megalist_parser.py
+++ megalist_parser.py
@@ -8,6 +8,7 @@ import os
 import re
 from lxml import etree
 from pathlib import Path
+from collections import defaultdict
 from PIL import Image
 
 # get megalist from github
@@ -24,6 +25,13 @@ for n in range(len(megalist)):
     break
 
 megalist = "\n".join(megalist[table_start:])
+
+input_data = Path("input")
+input_huds = defaultdict(dict)
+if input_data.exists():
+  for f in input_data.glob("*.json"):
+    with open(f) as file:
+      input_huds[f.stem] = json.load(file)
 
 huds = []
 
@@ -90,21 +98,35 @@ for hud in huds:
     continue
 
   image_links = get_links(hud["Screens"])
-  resource = image_links["Album"] if "Album" in image_links else image_links["Screen"]
+  album = image_links["Album"] if "Album" in image_links else image_links["Screen"]
 
   # Conditionally build the data
-  hud_meta = {}
-  hud_meta["name"] = hud_name
-  hud_meta["author"] = author
-  if steam_group or discord:
+  hud_meta_in = input_huds[hud_id]
+  if hud_meta_in.get("social"):
+    hud_meta = hud_meta_in
+  else:
+    hud_meta = {}
+    hud_meta["name"] = hud_name
+    hud_meta["author"] = author
     hud_meta["social"] = {}
     if steam_group:
       hud_meta["social"]["steam_group"] = steam_group
     if discord:
       hud_meta["social"]["discord"] = discord
-  hud_meta["repo"] = repo
-  hud_meta["hash"] = ""
-  hud_meta["resources"] = resource
+    hud_meta["repo"] = repo
+    hud_meta["hash"] = ""
+    if hud_meta_in.get("parent"):
+      hud_meta["parent"] = hud_meta_in["parent"]
+    hud_meta["resources"] = []
+
+  if album:
+      hud_meta["social"]["album"] = album
+
+  for key, value in hud_meta_in.items():
+    if key == "social":
+      continue
+    hud_meta[key] = value
+
   hud_data[hud_id] = hud_meta
 
 output = Path("./output/")
@@ -114,20 +136,16 @@ data_out.mkdir(exist_ok=True)
 data_res = output / "resources"
 data_res.mkdir(exist_ok=True)
 
-hud_res = {}
 
 for hud_id, hud in hud_data.items():
   hud_data_path = data_out / f"{hud_id}.json"
 
-  res = hud["resources"]
-  hud_res[hud_id] = res
-
   if hud_data_path.exists():
     continue
 
-  hud["resources"] = [f"{hud_id}-banner"]
+  hud["resources"] = hud["resources"] or [f"{hud_id}-banner"]
 
-  hud["hash"] = subprocess.check_output(["git", "ls-remote", hud["repo"], "HEAD"]).decode("utf-8").split()[0]
+  hud["hash"] = hud["hash"] or subprocess.check_output(["git", "ls-remote", hud["repo"], "HEAD"]).decode("utf-8").split()[0]
 
   with open(hud_data_path, "w") as f:
     json.dump(hud, f, indent=2)
@@ -138,19 +156,29 @@ imgur_headers = {
   'Accept': 'application/json'
 }
 
+def download_img(url, folder, final_path):
+  img_name = url.split('/')[-1]
+  img_path = folder / img_name
+  urllib.request.urlretrieve(url, img_path)
+  im = Image.open(img_path).convert("RGB")
+  img_path.unlink()
+  im.save(folder / f"{final_path}.webp", quality=75)
+
 for hud_id, hud in hud_data.items():
   data_res_id = data_res / hud_id
-  data_res_id.mkdir(exist_ok=True)
-
-  final_res_path = data_res_id / f"{hud_id}-banner.webp"
+  final_res_path = data_res_id
   if final_res_path.exists():
     continue
+  data_res_id.mkdir()
 
-  res = hud_res[hud_id]
+  res = hud["social"]["album"]
 
   # download the img
   if "camo.githubusercontent.com" in res or "i.imgur.com" in res:
     img_url = res
+    # single img, not an album
+    del hud["social"]["album"]
+    download_img(img_url, data_res_id, f"{hud_id}-banner")
   elif "imgur.com/a/" in res or "imgur.com/gallery/" in res or "imgur.com/album/" in res:
     res = res.replace("imgur.com/a/", "imgur.com/album/").replace("imgur.com/gallery/", "imgur.com/album/")
     imgur_res = "/".join(res.split("/")[-2:])
@@ -162,14 +190,22 @@ for hud_id, hud in hud_data.items():
       print(f"Failed to get imgur data for {hud_id}: {imgur_resp.text}")
       raise
     images = imgur_data["data"]["images"]
-    img_url = images[0]["link"]
+    for idx in range(min(4, len(images))):
+      img_link = images[idx]["link"]
+      if idx == 0:
+        download_img(img_link, data_res_id, f"{hud_id}-banner")
+      elif False:
+        res_name = f"{hud_id}-{idx}"
+        hud["resources"].append(res_name)
+        download_img(img_link, data_res_id, res_name)
+      
+    if False:
+      hud_data_path = data_out / f"{hud_id}.json"
+
+      with open(hud_data_path, "w") as f:
+        json.dump(hud, f, indent=2)
   else:
     print(f"Unknown resource type for {hud_id}: {res}")
     continue
 
-  img_name = img_url.split('/')[-1]
-  img_path = data_res_id / img_name
-  urllib.request.urlretrieve(img_url, img_path)
-  im = Image.open(img_path).convert("RGB")
-  img_path.unlink()
-  im.save(final_res_path, quality=75)
+  
